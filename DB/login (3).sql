eaeddd33afaecd89b2d9c5a9dc35ac677b5ac468
DrakonZX <TheDrakonZXD@gmail.com>
2019-12-02 13:10:20 -0200

    Troca de Banco de dados

--- DB/login (3).sql
+++ DB/login (3).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 28-Nov-2019 às 21:49
+-- Generation Time: 02-Dez-2019 às 16:09
 -- Versão do servidor: 10.1.37-MariaDB
 -- versão do PHP: 7.3.1
 
@@ -37,13 +37,18 @@ CREATE TABLE `carrinho` (
   `cliente_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
+
 --
--- Extraindo dados da tabela `carrinho`
+-- Estrutura da tabela `compras`
 --
 
-INSERT INTO `carrinho` (`id`, `quantidade`, `produto_id`, `cliente_id`) VALUES
-(5, 5, 2, 1),
-(6, 2, 12, 1);
+CREATE TABLE `compras` (
+  `id` int(11) NOT NULL,
+  `cliente_id` int(11) NOT NULL,
+  `gasto` decimal(6,2) NOT NULL,
+  `data` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -68,7 +73,11 @@ INSERT INTO `datas` (`id`, `data`, `email`, `hora`, `estado`) VALUES
 (2, '2019-11-16', 'TheDrakonZXD@gmail.com', '14:10:29', 'logado'),
 (3, '2019-11-16', 'TheDrakonZXD@gmail.com', '14:10:41', 'deslogado'),
 (4, '2019-11-16', 'TheDrakonZXD@gmail.com', '14:32:52', 'logado'),
-(5, '2019-11-28', 'TheDrakonZXD@gmail.com', '14:49:48', 'logado');
+(5, '2019-11-28', 'TheDrakonZXD@gmail.com', '14:49:48', 'logado'),
+(6, '2019-11-30', 'TheDrakonZXD@gmail.com', '00:07:06', 'logado'),
+(7, '2019-11-30', 'TheDrakonZXD@gmail.com', '13:50:46', 'logado'),
+(8, '2019-12-01', 'TheDrakonZXD@gmail.com', '12:44:54', 'logado'),
+(9, '2019-12-01', 'TheDrakonZXD@gmail.com', '19:10:33', 'logado');
 
 -- --------------------------------------------------------
 
@@ -138,9 +147,9 @@ INSERT INTO `produtos_airsoft` (`id`, `nome`, `preco`, `preco_antigo`, `parcelas
 (1, 'SHOTGUN AIRSOFT MOSSBERG 500 ', '500.00', '0.00', '50.00', '315.00', 'ActionX airsoft', 'img_produtos/Imagens_correta/5.jpg', 'img_produtos/Imagens_correta/5_2.jpg', 'img_produtos/Imagens_correta/5_3.jpg', 'Escopetas', 0, 0, '0.00', '14.00', 'Spring', 'Manual', '', '6mm', '98.00', '13.00', '1x Fake Red-dot', 'Não contém', 'Polímero e Metal', '', 'Airsoft', 'escopeta.php', 'airsoft.php', 0, ''),
 (2, 'RIFLE AEG M4A1 CM507', '999.00', '1350.00', '99.90', '999.00', 'slin', 'img_produtos/Imagens_correta/14.jpg', 'img_produtos/Imagens_correta/14_2.jpg', 'img_produtos/Imagens_correta/14_3.jpg', 'Rifles', 0, 1, '0.00', '15.24', 'Pressão', 'Manual', '', '4.3', '58.00', '15.00', 'Não contém', 'Não contém', 'Polimero e Metal', '', 'Airsoft', 'rifle.php', 'airsoft.php', 1, 'img/a.png'),
 (3, 'PISTOLA 247 KWC', '299.00', '499.00', '59.80', '269.00', 'slin', 'img_produtos/Imagens_correta/11.jpg', 'img_produtos/Imagens_correta/11_2.jpg', 'img_produtos/Imagens_correta/11_3.jpg', 'Pistolas', 1, 0, '0.00', '0.00', 'Pressão', 'Manual', '', '4.3', '36.00', '6.00', 'Não contém', 'Não contém', 'Polímero e Metal', '', 'Airsoft', 'pistola.php', 'airsoft.php', 1, 'img/c.png'),
-(4, 'PISTOLA 1911 GBB CO2', '999.00', '2191.00', '99.00', '899.10', 'slin', 'img_produtos/Imagens_correta/12.jpg', 'img_produtos/Imagens_correta/12_2.jpg', 'img_produtos/Imagens_correta/12_3.jpg', 'Pistolas CO2', 0, 0, '0.00', '0.00', 'Gás CO2', 'Manual', '', '4.3', '46.00', '7.00', 'Não contém', 'Contém', 'Polímero e Metal', '', 'Airsoft', 'pistola.php', 'airsoft.php', 0, ''),
-(5, 'PISTOLA GLOCK GBB  GREEN GAS R17', '999.00', '1320.00', '99.00', '899.00', 'slin', 'img_produtos/Imagens_correta/13.jpg', 'img_produtos/Imagens_correta/13_2.jpg', 'img_produtos/Imagens_correta/13_3.jpg', 'Pistolas GBB', 1, 1, '0.00', '0.00', 'Gás CO2', '', '', '', '0.00', '0.00', '', '', '', '', 'Airsoft', 'pistola.php', 'airsoft.php', 0, ''),
-(6, 'BERETTA AEP UMAREX', '890.00', '1250.00', '89.00', '801.00', 'slin', 'img_produtos/Imagens_correta/10.jpg', 'img_produtos/Imagens_correta/10_2.jpg', 'img_produtos/Imagens_correta/10_3.jpg', 'Pistolas Elétrica', 0, 0, '0.00', '0.00', '', '', '', '', '0.00', '0.00', '', '', '', '', 'Airsoft', 'pistola.php', 'airsoft.php', 0, ''),
+(4, 'PISTOLA 1911 GBB CO2', '999.00', '2191.00', '99.00', '899.10', 'slin', 'img_produtos/Imagens_correta/12.jpg', 'img_produtos/Imagens_correta/12_2.jpg', 'img_produtos/Imagens_correta/12_3.jpg', 'Pistolas ', 0, 0, '0.00', '0.00', 'Gás CO2', 'Manual', '', '4.3', '46.00', '7.00', 'Não contém', 'Contém', 'Polímero e Metal', '', 'Airsoft', 'pistola.php', 'airsoft.php', 0, ''),
+(5, 'PISTOLA GLOCK GBB  GREEN GAS R17', '999.00', '1320.00', '99.00', '899.00', 'slin', 'img_produtos/Imagens_correta/13.jpg', 'img_produtos/Imagens_correta/13_2.jpg', 'img_produtos/Imagens_correta/13_3.jpg', 'Pistolas ', 1, 1, '0.00', '0.00', 'Gás CO2', '', '', '', '0.00', '0.00', '', '', '', '', 'Airsoft', 'pistola.php', 'airsoft.php', 0, ''),
+(6, 'BERETTA AEP UMAREX', '890.00', '1250.00', '89.00', '801.00', 'slin', 'img_produtos/Imagens_correta/10.jpg', 'img_produtos/Imagens_correta/10_2.jpg', 'img_produtos/Imagens_correta/10_3.jpg', 'Pistolas', 0, 0, '0.00', '0.00', '', '', '', '', '0.00', '0.00', '', '', '', '', 'Airsoft', 'pistola.php', 'airsoft.php', 0, ''),
 (7, 'SNIPER GREEN GAS G86B', '3799.00', '4591.20', '379.00', '3419.10', 'slin', 'img_produtos/Imagens_correta/16.jpg', 'img_produtos/Imagens_correta/16_2.jpg', 'img_produtos/Imagens_correta/16_3.jpg', 'Rifles', 1, 1, '0.00', '0.00', '', '', '', '', '0.00', '0.00', '', '', '', '', 'Airsoft', 'rifle.php', 'airsoft.php', 1, 'img/b.png'),
 (8, 'RIFLE M62', '719.10', '1199.00', '79.90', '719.10', 'slin', 'img_produtos/Imagens_correta/15.jpg', 'img_produtos/Imagens_correta/15_2.jpg', 'img_produtos/Imagens_correta/15_3.jpg', 'Rifles', 0, 0, '0.00', '0.00', '', '', '', '', '0.00', '0.00', '', '', '', '', 'Airsoft', 'rifle.php', 'airsoft.php', 0, ''),
 (9, 'MARCADOR PAINTBALL RIFLE LASER TAG EAGLE  EYE SYSTEM – PRETO', '4850.40', '0.00', '485.04', '4122.84', 'slin', 'img_produtos/Imagens_correta/8.jpg', 'img_produtos/Imagens_correta/8_2.jpg', '', 'Marcadores', 0, 1, '0.00', '0.00', '', '', '', '', '0.00', '0.00', '', '', '', '', 'Paintball', 'paintball.php', 'paintball.php', 0, ''),
@@ -150,6 +159,19 @@ INSERT INTO `produtos_airsoft` (`id`, `nome`, `preco`, `preco_antigo`, `parcelas
 (13, 'MUNIÇÃO AIRSOFT TOKYO MARUI BBS 0.25g - 1300 UNIDADES', '62.91', '0.00', '6.29', '53.47', 'slin', 'img_produtos/Imagens_correta/2.jpg', '', '', 'Acessórios', 1, 1, '0.00', '0.00', '', '', '', '', '0.00', '0.00', '', '', '', '', 'Airsoft', 'acessorios_airsoft.php', 'airsoft.php', 0, ''),
 (14, 'ARCO CRUZ JANDÃO CROSSBOW HWXR - 33 35 - 60 180', '2839.94', '0.00', '283.99', '2413.94', 'slin', 'img_produtos/Imagens_correta/3.jpg', 'img_produtos/Imagens_correta/3_2.jpg', 'img_produtos/Imagens_correta/3_3.jpg', 'Arcos', 0, 0, '0.00', '0.00', '', '', '', '', '0.00', '0.00', '', '', '', '', 'Arquearia', 'arquearia.php', 'arquearia.php', 0, ''),
 (15, 'BALESTRA CHACE SUN JANDÃO 2009F - 176', '2404.56', '0.00', '240.56', '2043.88', 'slin', 'img_produtos/Imagens_correta/4.jpg', 'img_produtos/Imagens_correta/4.2jpg', 'img_produtos/Imagens_correta/4_3.jpg', 'Balestras', 0, 0, '0.00', '0.00', '', '', '', '', '0.00', '0.00', '', '', '', '', 'Arquearia', 'arquearia.php', 'arquearia.php', 0, '');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `soma_total`
+--
+
+CREATE TABLE `soma_total` (
+  `id` int(11) NOT NULL,
+  `cliente_id` int(11) NOT NULL,
+  `produto_id` int(11) NOT NULL,
+  `soma` decimal(6,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -197,6 +219,12 @@ ALTER TABLE `carrinho`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `compras`
+--
+ALTER TABLE `compras`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `datas`
 --
 ALTER TABLE `datas`
@@ -215,6 +243,12 @@ ALTER TABLE `produtos_airsoft`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `soma_total`
+--
+ALTER TABLE `soma_total`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `usuario`
 --
 ALTER TABLE `usuario`
@@ -228,19 +262,31 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT for table `carrinho`
 --
 ALTER TABLE `carrinho`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `compras`
+--
+ALTER TABLE `compras`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `datas`
 --
 ALTER TABLE `datas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `filtro`
 --
 ALTER TABLE `filtro`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `soma_total`
+--
+ALTER TABLE `soma_total`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `usuario`
